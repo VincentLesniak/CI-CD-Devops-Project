29110e65684e258124180bb96cba62915a4f4eca
Vincent Lesniak <vincentlesniakpro@gmail.com>
2026-04-18 21:59:06 +0200

    fix: forcing vite

---
+++ jeu-1-two-ships/vite.config.js
@@ -0,0 +1,9 @@
+//début de l'intégration du fichier de réglage vite
+//importe la fonction requise
+import { defineConfig } from 'vite'
+
+//prépare l'exportation des paramètres
+export default defineConfig({
+//force la lecture des données depuis le répertoire courant
+  base: './'
+})
